e9a0ba2c9f0c05a3de86b089ff7a452b49d83c58
Youssef97dev <youssefmoussafi97@gmail.com>
2025-05-26 10:21:17 +0100

    project starter

---
+++ src/components/Loader.jsx
@@ -0,0 +1,31 @@
+"use client";
+import { useState, useEffect } from "react";
+import { BounceLoader } from "react-spinners";
+
+const Loader = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Simulate loading time
+    const timer = setTimeout(() => setIsClient(true), 2500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {isClient ? (
+        <div className="relative">
+          {/*<div className="fixed bottom-5 left-0 w-full block lg:hidden z-10">
+            <Buttons />
+          </div>*/}
+        </div>
+      ) : (
+        <div className="fixed inset-0 z-40 flex items-center justify-center bg-primary">
+          <BounceLoader color="#d7b072" size={100} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Loader;
